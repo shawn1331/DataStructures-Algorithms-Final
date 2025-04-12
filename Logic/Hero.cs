3af6af9acfa4d0feb20d54ae0965c7a660e299a9
Shawn Miner <shawnmin13@gmail.com>
2025-04-11 21:05:44 -0600

    Add project files.

---
+++ Logic/Hero.cs
@@ -0,0 +1,19 @@
+﻿namespace Logic;
+public class Hero
+{
+    public string Name { get; }
+    public int Health { get; }
+    public int Agility { get; }
+    public int Wisdon { get; }
+    public Queue<string> Inventory { get; }
+
+    public Hero(string name)
+    {
+        Name = name;
+        Health = 20;
+        Inventory = new();
+        Inventory.Enqueue("Health Potion");
+        Inventory.Enqueue("Sword");
+
+    }
+}
